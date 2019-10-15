a01b661c6438e706e9d784543f21a5797a29bcc6
vagrant <vagrant@homestead>
2019-10-15 22:19:34 +0000

    added register and logout to AuthController and routes

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use App\User;
 
 class AuthController extends Controller
 {
@@ -33,5 +35,29 @@ class AuthController extends Controller
 
       return response()->json('Something went wrong on the server.', $e->getCode());
     }
+  }
+
+  public function register(Request $request)
+  {
+    $request->validate([
+      'name' => ['required', 'string', 'max:255'],
+      'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+      'password' => ['required', 'string', 'min:8'],
+    ]);
+
+    return User::create([
+      'name' => $request->name,
+      'email' => $request->email,
+      'password' => Hash::make($request->password),
+    ]);
+  }
+
+  public function logout()
+  {
+    auth()->user()->tokens->each(function ($token, $key){
+      $token->revoke();
+    });
+
+    return response()->json('Logged out successfully.', 200);
   }
 }
