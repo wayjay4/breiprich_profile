9918b56116331ae39a044ee14f839963b3f1ac58
Waylon Dixon <wayjay4@yahoo.com>
2019-10-29 22:52:35 +0000

    added logout functionality to dashboard layout

--- resources/views/layouts/dashboard/app.blade.php
+++ resources/views/layouts/dashboard/app.blade.php
@@ -23,12 +23,25 @@
   <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm fixed-top">
     <i class="material-icons">account_balance_wallet</i>
     <h5 class="my-0 mr-md-auto font-weight-normal">{{ config('app.name') }}</h5>
+
     <nav class="my-2 my-md-0 mr-md-3">
       <a class="p-2 text-dark" href="#">My Account</a>
       <a class="p-2 text-dark" href="#">Settings</a>
       <a class="p-2 text-dark" href="#">Support</a>
     </nav>
-    <a class="btn btn-outline-primary" href="#">Sign out</a>
+
+    <div>
+      <a
+        class="btn btn-outline-primary"
+        href="{{ route('logout') }}"
+        onclick="event.preventDefault(); document.getElementById('logout-form').submit();"
+      >
+        {{ __('Logout') }}
+      </a>
+      <form id="logout-form" action="{{ route('logout') }}" method="POST" hidden>
+          @csrf
+      </form>
+    </div>
   </div>
 
 
